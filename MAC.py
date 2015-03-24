682df87e866e450591bd388552f53695c2fbd6f9
gemepio <mhlt41621@gmail.com>
2015-03-24 17:02:47 -0400

    Corner stone is here

---
+++ MAC.py
@@ -0,0 +1,16 @@
+#Marker and Cell Method for Solving Incompressible Flow
+#import matplotlib
+#import scipy
+#import math
+#import numpy
+
+def GridGeneration():
+	#print("This function generate the MAC grid")
+
+def main():
+	GridGeneration()
+	
+	print("The Mac grid has been generated")
+
+if __name__ == '__main__':
+	main()
